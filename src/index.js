d9872c9b49140af9d1ddcf59fd0282fcae0f000f
Anna Samoilova <samoilova0299@gmail.com>
2023-11-11 23:47:55 +0200

    add initial settings

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { GlobalStyles } from './commonStyle/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyles />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 );
