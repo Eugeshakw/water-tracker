957e1617fb3fceb9504b2f2155c927fa2f657667
Eugeshakw <eugeshakw@gmail.com>
2023-11-12 11:45:35 +0200

    uppRoute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,24 @@
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
-import { PublicRoute } from './publickRoute.jsx';
+// import { PublicRoute } from './publickRoute.jsx';
 import Layout from '../components/Layout/Loyout'
 // import { PrivateRoute } from './privateRoute';
 export const App = () => {
   return (
+    
+   
     <Routes>
-      <Route path='/' element={<Layout/>}>
-
+      <Route path='/' element={<Layout />}>
         <Route index element={'main'} />
         <Route path="/signup" element={'SignUpPage'} />
-        <Route path="/signin" element={'SigninPage'} />
-
-        <Route
-          path="register"
-          element={<PublicRoute redirectTo="/" component={'register'} />}
-        />
-
-        <Route
-          path="dashboard"
-          element={
-          ""
-          }
-        />
+        <Route path="/signin" element={'SignInPage'} />
+        {/* <Route path="register" element={<PublicRoute redirectTo="/" component={'Register'} />} /> */}
+        <Route path="dashboard" element={''} />
         <Route path='/forgot-password' element={'ForgotPasswordPage'}/>
-
-        
       </Route>
-       
-      
     </Routes>
+  
+   
+    
   );
 };
