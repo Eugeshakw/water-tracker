258dfd3443d48f39954878d697e76661aed027e2
Sergii Podriezienko <podreenko1986@gmail.com>
2023-11-12 14:11:53 +0200

    add: days general preview
    add: days general preview

--- src/components/DaysGeneralStats/DaysGeneralStats.jsx
+++ src/components/DaysGeneralStats/DaysGeneralStats.jsx
@@ -5,4 +5,21 @@ const DaysGeneralStats = function ({
   dailyNorma,
   fullfilmentOfDailyNorma,
   howManyServings,
-}) {};
+}) {
+  return (
+    <div className={css.divDaysGeneralStats}>
+      <p className={css.dateOfMonthParagrah}>{dateOfMonth}</p>
+      <p className={css.dailyNormaParagrah}>
+        Daily norma: <span className={css.dailyNormaSpan}>{dailyNorma}</span>
+      </p>
+      <p className={css.fullfilmentParagrah}>
+        Fulfillment of the daily norm:
+        <span className={css.fullfilmentSpan}>{fullfilmentOfDailyNorma}</span>
+      </p>
+      <p className={css.howManyServingsParagrah}>
+        How many servings of water:
+        <span className={css.howManyServingsSpan}>{howManyServings}</span>
+      </p>
+    </div>
+  );
+};
