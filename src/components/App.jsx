068083090d280d5ff9cae74da86c933ccf1fac85
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2023-11-12 15:27:15 +0200

    front-end-part-of-forgot-pass-page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,21 @@
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Layout/Loyout';
+import './ForgotPasswordPage';
+import ForgotPasswordPage from './ForgotPasswordPage';
 
-import { PublicRoute } from './publickRoute.jsx';
-import Layout from '../components/Layout/Loyout'
-// import { PrivateRoute } from './privateRoute';
 export const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-
-        <Route index element={'main'} />
-        <Route path="/signup" element={'SignUpPage'} />
-        <Route path="/signin" element={'SigninPage'} />
-
-        <Route
-          path="register"
-          element={<PublicRoute redirectTo="/" component={'register'} />}
-        />
-
-        <Route
-          path="dashboard"
-          element={
-          ""
-          }
-        />
-        <Route path='/forgot-password' element={'ForgotPasswordPage'}/>
-
-        
-      </Route>
-       
-      
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={'main'} />
+          <Route path="/signup" element={'SignUpPage'} />
+          <Route path="/signin" element={'SignInPage'} />
+          {/* <Route path="register" element={<PublicRoute redirectTo="/" component={'Register'} />} /> */}
+          <Route path="dashboard" element={''} />
+          <Route path='/forgot-password' element={<ForgotPasswordPage/>} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
