415591f68e7463a20ce134fd2563ec1e0695708f
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-11-12 22:18:41 +0100

    first commit

--- src/components/Layout/Loyout.jsx
+++ src/components/Layout/Loyout.jsx
@@ -1,16 +1,13 @@
-import { Suspense } from "react"
-import { Outlet } from "react-router-dom"
-import Header from "components/header/header"
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from 'components/header/header';
 const Layout = () => {
-
-    return (
-        <Suspense>
-            <Header/>
-            <Outlet/>
-   
-        </Suspense>
-    )
-
-}
+  return (
+    <Suspense>
+      <Header />
+      <Outlet />
+    </Suspense>
+  );
+};
 
 export default Layout;
