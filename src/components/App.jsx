dd6509a299ec509d84f71fe9937052f7394fa642
Sergii Podriezienko <podreenko1986@gmail.com>
2023-11-13 11:04:13 +0200

    add: changes before pull request
    add: changes before pull request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,22 @@
-//import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // import { PublicRoute } from './publickRoute.jsx';
-//import Layout from '../components/Layout/Loyout';
+import Layout from '../components/Layout/Loyout';
 // import { PrivateRoute } from './privateRoute';
-//import WhyDrinkWater from './WhyDrinkWater/WhyDrinkWater';
-import DaysGeneralStats from './DaysGeneralStats/DaysGeneralStats';
+import WhyDrinkWater from './WhyDrinkWater/WhyDrinkWater';
 
 export const App = () => {
   return (
-    <DaysGeneralStats
-      dateOfMonth="8.3"
-      dailyNorma="1.2"
-      fullfilmentOfDailyNorma="1.3"
-      howManyServings="5.2"
-    />
-    // <WhyDrinkWater />
-    //    <Routes>
-    //      <Route path="/" element={<Layout />}>
-    //       <Route index element={'main'} />
-    //        <Route path="/signup" element={'SignUpPage'} />
-    //        <Route path="/signin" element={'SignInPage'} />
-    //        {/* <Route path="register" element={<PublicRoute redirectTo="/" component={'Register'} />} /> */}
-    //        <Route path="dashboard" element={''} />
-    //        <Route path="/forgot-password" element={'ForgotPasswordPage'} />
-    //        <Route path="whydrinkwater" element={<WhyDrinkWater />} />
-    //      </Route>
-    //    </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={'main'} />
+        <Route path="/signup" element={'SignUpPage'} />
+        <Route path="/signin" element={'SignInPage'} />
+        {/* <Route path="register" element={<PublicRoute redirectTo="/" component={'Register'} />} /> */}
+        <Route path="dashboard" element={''} />
+        <Route path="/forgot-password" element={'ForgotPasswordPage'} />
+        <Route path="whydrinkwater" element={<WhyDrinkWater />} />
+      </Route>
+    </Routes>
   );
 };
--- src/components/WhyDrinkWater/WhyDrinkWater.jsx
+++ src/components/WhyDrinkWater/WhyDrinkWater.jsx
@@ -7,41 +7,41 @@ const WhyDrinkWater = function () {
       <h2 className={css.headerWhyDrinkWater}>Why drink water</h2>
       <ul>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>
             Supply of nutrients to all organs
           </p>
         </li>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>Providing oxygen to the lungs</p>
         </li>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>
             Maintaining the work of the heart
           </p>
         </li>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>
             Release of processed substances
           </p>
         </li>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>
             Ensuring the stability of the internal environment
           </p>
         </li>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>
             Maintaining within the normal temperature
           </p>
         </li>
         <li className={css.itemOfListWhyDrinkWater}>
-          <img className={css.imgIcon} src={img} />
+          <img className={css.imgIcon} src={img} alt="icon item" />
           <p className={css.itemWhyDrinkWater}>
             Maintaining an immune system capable of resisting disease
           </p>
