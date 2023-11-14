ac0325af8c812eb4dccbf4046b8a49df9f348dfd
Sergii Podriezienko <podreenko1986@gmail.com>
2023-11-14 09:58:36 +0200

    add: close modal days general stats
    add: close modal days general stats

--- src/components/DaysGeneralStats/DaysGeneralStats.jsx
+++ src/components/DaysGeneralStats/DaysGeneralStats.jsx
@@ -1,5 +1,6 @@
 import css from './DaysGeneralStats.module.css';
 import img from '../DaysGeneralStats/icons/button_svg.svg';
+import { useState } from 'react';
 
 const DaysGeneralStats = function ({
   dateOfMonth,
@@ -7,26 +8,39 @@ const DaysGeneralStats = function ({
   fullfilmentOfDailyNorma,
   howManyServings,
 }) {
+  const [isClose, setIsClose] = useState(false);
+
+  const onClickButtonClose = function () {
+    setIsClose(true);
+  };
+
   return (
-    <div className={css.divDaysGeneralStats}>
-      <div className={css.dateOfMonthGroup}>
-        <p className={css.dateOfMonthParagrah}>{dateOfMonth}</p>
-        <button className={css.buttonClose}>
-          <img className={css.imgButtonClose} src={img} alt="button icon" />
-        </button>
-      </div>
-      <p className={css.dailyNormaParagrah}>
-        Daily norma: <span className={css.dailyNormaSpan}>{dailyNorma}</span>
-      </p>
-      <p className={css.fullfilmentParagrah}>
-        Fulfillment of the daily norm:{' '}
-        <span className={css.fullfilmentSpan}>{fullfilmentOfDailyNorma}</span>
-      </p>
-      <p className={css.howManyServingsParagrah}>
-        How many servings of water:{' '}
-        <span className={css.howManyServingsSpan}>{howManyServings}</span>
-      </p>
-    </div>
+    <>
+      {isClose === false && (
+        <div className={css.divDaysGeneralStats}>
+          <div className={css.dateOfMonthGroup}>
+            <p className={css.dateOfMonthParagrah}>{dateOfMonth}</p>
+            <button className={css.buttonClose} onClick={onClickButtonClose}>
+              <img className={css.imgButtonClose} src={img} alt="button icon" />
+            </button>
+          </div>
+          <p className={css.dailyNormaParagrah}>
+            Daily norma:{' '}
+            <span className={css.dailyNormaSpan}>{dailyNorma}</span>
+          </p>
+          <p className={css.fullfilmentParagrah}>
+            Fulfillment of the daily norm:{' '}
+            <span className={css.fullfilmentSpan}>
+              {fullfilmentOfDailyNorma}
+            </span>
+          </p>
+          <p className={css.howManyServingsParagrah}>
+            How many servings of water:{' '}
+            <span className={css.howManyServingsSpan}>{howManyServings}</span>
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
