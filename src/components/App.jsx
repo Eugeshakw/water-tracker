1f226d8d20bc74fb2740f6884e38432183d41ea6
Sergii Podriezienko <podreenko1986@gmail.com>
2023-11-15 11:58:24 +0200

    add: refactoring styled components
    add: refactoring styled components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,16 @@ import WhyDrinkWater from './WhyDrinkWater/WhyDrinkWater';
 
 export const App = () => {
   return (
-    <WhyDrinkWater />
-    //  <Routes>
-    //    <Route path="/" element={<Layout />}>
-    //      <Route index element={'main'} />
-    //      <Route path="/signup" element={'SignUpPage'} />
-    //      <Route path="/signin" element={'SignInPage'} />
-    //      {/* <Route path="register" element={<PublicRoute redirectTo="/" component={'Register'} />} /> */}
-    //      <Route path="dashboard" element={''} />
-    //      <Route path="/forgot-password" element={'ForgotPasswordPage'} />
-    //      <Route path="whydrinkwater" element={<WhyDrinkWater />} />
-    //    </Route>
-    //  </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={'main'} />
+        <Route path="/signup" element={'SignUpPage'} />
+        <Route path="/signin" element={'SignInPage'} />
+        {/* <Route path="register" element={<PublicRoute redirectTo="/" component={'Register'} />} /> */}
+        <Route path="dashboard" element={''} />
+        <Route path="/forgot-password" element={'ForgotPasswordPage'} />
+        <Route path="whydrinkwater" element={<WhyDrinkWater />} />
+      </Route>
+    </Routes>
   );
 };
