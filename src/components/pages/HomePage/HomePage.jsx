2bae0c7abed3d19d1a74b69a67a8897fa0bc19af
Eugeshakw <eugeshakw@gmail.com>
2023-11-24 15:31:44 +0200

    uppddate Home Page

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -9,22 +9,18 @@ import {
   Wrapper,
   BackImage,
   ConImg,
-  DecorateImg,
 } from './HomePage.styled';
 import WaterList from 'components/DailyNorma/WaterList/WaterList';
-
 import Backdrop from 'components/Backdrop/Backdrop';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { setModalType, setOpenModal } from 'redux/modals/Slice';
 import AmountOfWater from 'components/AmountOfWater/AmountOfWater';
 import DailyNormaModal from 'components/MyDailyNorma/DailyNormaModal';
-
+import AddWater from 'components/AddWater/AddWater';
 const HomePage = () => {
   const isOpenModal = useSelector(state => state.modals.isOpenModal);
   const modalType = useSelector(state => state.modals.modalType);
   const dispatch = useDispatch();
-
   const setActive = text => {
     dispatch(setOpenModal(true));
     dispatch(setModalType(text));
@@ -41,33 +37,39 @@ const HomePage = () => {
           <AmountOfWater />
         </Backdrop>
       )}
-
+      {isOpenModal && modalType === 'AddWater' && (
+        <Backdrop>
+          <AddWater />
+        </Backdrop>
+      )}
       <div className="container">
-        <DecorateImg>
-          <Container>
-            <NormaContainer>
-              <DailyNorma setActive={setActive} />
-              <ConImg>
-                <BackImage />
-              </ConImg>
-              <Wrapper>
-                <ContentWrapper>
-                  <ProgressiveBar />
-                </ContentWrapper>
-                <ContentWrapper>
-                  <Button type="button">
-                    <Icon />
-                    Add Water
-                  </Button>
-                </ContentWrapper>
-              </Wrapper>
-            </NormaContainer>
-            <WaterList />
-          </Container>
-        </DecorateImg>
+        <Container>
+          <NormaContainer>
+            <DailyNorma setActive={setActive} />
+            <ConImg>
+              <BackImage />
+            </ConImg>
+            <Wrapper>
+              <ContentWrapper>
+                <ProgressiveBar />
+              </ContentWrapper>
+              <ContentWrapper>
+                <Button
+                  type="button"
+                  onClick={() => {
+                    setActive('AddWater');
+                  }}
+                >
+                  <Icon />
+                  Add Water
+                </Button>
+              </ContentWrapper>
+            </Wrapper>
+          </NormaContainer>
+          <WaterList setActive={setActive} />
+        </Container>
       </div>
     </>
   );
 };
-
 export default HomePage;
